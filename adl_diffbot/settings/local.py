a939f124f8a1570d2ad54b622c527d39125e948b
iamriel <me@iamriel.com>
2014-03-10 12:23:32 +0800

    Adds main application and database settings

--- adl_diffbot/settings/local.py
+++ adl_diffbot/settings/local.py
@@ -2,3 +2,20 @@ from .base import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adl_diffbot',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+            'autocommit': True,
+        }
+    }
+}
--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Media(models.Model):
+    caption = models.CharField(max_length=50, blank=True)
+    link = models.URLField(max_length=500)
+
+    class Meta:
+        verbose_name_plural = 'Media'
+
+    def __unicode__(self):
+        return '{} - {}'.format(self.caption, self.link)
+
+
+class Product(models.Model):
+    product_id = models.CharField(max_length=50)
+    link = models.URLField(max_length=500)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    offer_price = models.FloatField(blank=True, null=True)
+    regular_price = models.FloatField(blank=True, null=True)
+    merchant = models.CharField(max_length=20, blank=True)
+    media = models.ForeignKey('Media', blank=True, null=True)
+
+    def __unicode__(self):
+        return '{} - {}'.format(self.product_id, self.title)
+
+
