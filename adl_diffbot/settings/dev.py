453e3e1eee3c02b2c3ad69921890f66c34bb3b54
iamriel <me@iamriel.com>
2014-03-11 13:02:10 +0800

    Adds dev settings

--- adl_diffbot/settings/dev.py
+++ adl_diffbot/settings/dev.py
@@ -1,1 +1,21 @@
 from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adl_diffbot',
+        'USER': 'adl_postgres',
+        'PASSWORD': 'Alldealsleak123',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+            'autocommit': True,
+        }
+    }
+}
