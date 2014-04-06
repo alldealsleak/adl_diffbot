606bba571863cea996e550b395e88acc428d6697
iamriel <me@iamriel.com>
2014-04-07 02:49:31 +0800

    Adds Merchant.keywords

--- main/models.py
+++ main/models.py
@@ -8,6 +8,7 @@ COUNTRY_CHOICES = (
 
 class Merchant(models.Model):
     name = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
